002f0cd7541c05454c8393bc2877dee29fd99e81
Sedogrif <davo.sochi@gmail.com>
2025-12-10 07:45:34 -0600

    repasando funciones avanzadas

--- Programacion 6.6.25/JS-course/scripts/12-advanced-functions.js
+++ Programacion 6.6.25/JS-course/scripts/12-advanced-functions.js
@@ -1,5 +1,6 @@
-/*function greeting() {
-  console.log('hello');
+/*
+function greeting() {
+  console.log("Hello");
 }
 greeting();
 
@@ -7,7 +8,7 @@ greeting();
 const num = 2;
 
 const function1 = function() {
-  console.log('hello2');
+  console.log("Hello2");
 };
 
 
@@ -15,54 +16,45 @@ console.log(function1);
 console.log(typeof function1);
 function1();
 
-
 const object1 = {
   num: 2,
   fun: function() {
     console.log('hello3');
   }
 };
+
 object1.fun();
+
+
 
 function display(param) {
   console.log(param);
 }
+
 display(2);
 
 
 function run(param) {
-  param();
+ param();
 }
 run(function() {
   console.log('hello4')
 });
-*/
-/*
-setTimeout(function() {
-  console.log('timeout');
-  console.log('timeout2');
-}, 3000);
 
-
-console.log('next line');
-
-
-setInterval(function() {
-  console.log('Interval')
-}, 3000);
 */
 
-[
-  'make dinner',
-  'wash dishes',
-  'watch youtube'
-].forEach(function(value, index) {
-  if (value === 'wash dishes') {
-    return;
-  }
-
-  console.log(index);
-  console.log(value);
-});
+  setTimeout(function() {
+    console.log("timeout")
+    
+  }, 3000);
 
 
+  console.log('next line');
+  
+
+  setInterval(function() {
+    console.log('Interval');
+  }, 2000);
+
+
+  console.log('next line 2');
